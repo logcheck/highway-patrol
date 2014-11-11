acfdc336c86fd874941ac2dedf5e3ec703f9e972
Benjamin Ragheb <ben@benzado.com>
2014-11-10 19:09:57 -0500

    update install.sh to actually work, maybe

--- install.sh
+++ install.sh
@@ -2,7 +2,11 @@
 
 # Run as root
 
+install -o root -g wheel -m 755 -d /usr/local/bin/
+install -o root -g wheel -m 755 -d /usr/local/etc/
+
 install -o root -g wheel -m 400 highway-patrol.conf /usr/local/etc/
 install -o root -g wheel -m 500 highway-patrol.sh   /usr/local/bin/
 install -o root -g wheel -m 400 com.logcheck.highway-patrol.plist /Library/LaunchDaemons/
-launchctl load com.logcheck.highway-patrol
+
+launchctl load -w /Library/LaunchDaemons/com.logcheck.highway-patrol.plist
